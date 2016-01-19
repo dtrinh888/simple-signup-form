1a859595860d1335678804f0170049fec2e33fe5
dtrinh888 <dtrinh888@gmail.com>
2016-01-18 19:02:21 -0800

    simple signup

---
+++ app.js
@@ -0,0 +1,10 @@
+var app = angular.module('myApp', []);
+
+app.directive('optIn', function(){
+	return {
+		restrict: 'E',
+		transclude: true,
+		template: '<opt-in><div class="brand-logo" ng-transclude><img src="http://empoweredteamleaders.com/wp-content/uploads/2013/04/sign_up_blue.png" /></div></opt-in>',
+		replace: true
+	};
+});
