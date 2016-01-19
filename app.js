3f15e6a147604d2998a00981ae197b33e0a801d5
dtrinh888 <dtrinh888@gmail.com>
2016-01-18 23:39:15 -0800

    finished css

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var app = angular.module('myApp', []);
 
 app.directive('optIn', function(){
 	return {
+		templateUrl: './opt-in.html',
 		restrict: 'E',
 		transclude: true,
-		template: '<opt-in><div class="brand-logo col-md-6" ng-transclude><img src="http://empoweredteamleaders.com/wp-content/uploads/2013/04/sign_up_blue.png" /></div></opt-in>',
-		replace: true
+		replace: false
 	};
 });
